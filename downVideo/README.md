7d6fc653cb32e1f2f0a17234f4bfb7253991a675
Rodrigo Lopes <rjr89000@gmail.com>
2023-03-09 15:17:16 -0300

    start

---
+++ downVideo/README.md
@@ -0,0 +1,6 @@
+# Projeto fase inicial (09/03/2023) - Ferramentas para o dia a dia! 
+
+## Projeto construído em Python
+
+
+*By: Rodrigo Lopes*
--- downVideo/donwVideo.py
+++ downVideo/donwVideo.py
@@ -1,10 +1,10 @@
 from pytube import YouTube
 
-link = input('Digite o link do video')
+link = input('Digite o link do video: ')
 
 video = YouTube(link)
 
-print('Selecione um formato:')
+print('Selecione um formato: ')
 for stream in video.streams:
     print(stream)
 
