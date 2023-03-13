4dd55f42b7ba14986140071ff1c692b06fa34bc3
Rodrigo Lopes <rjr89000@gmail.com>
2023-03-13 14:46:13 -0300

    GPT

---
+++ integraçãoGPT/app.py
@@ -0,0 +1,5 @@
+from chatgptonic import ChatGPT
+
+chat = ChatGPT('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')
+response = chat.just_chat("quantas vezes o vasco caiu pra segunda divisão")
+print(response)
